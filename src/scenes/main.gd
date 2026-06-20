0fc0745cbc621e705a5b642a4dea8478a26a998d
alstocras <alstoperiment@gmail.com>
2026-06-20 12:30:11 +0000

    allowed the player to place objects

--- src/scenes/main.gd
+++ src/scenes/main.gd
@@ -2,7 +2,12 @@ extends Node2D;
 
 var basicExtractor = preload("res://src/scenes/extractor.tscn");
 var mousePos;
+var obj;
 @onready var playerScene = $Player
 
-func _ready():
-	playerScene.placeExtractor.connect(playerScene.placeExtractor());
+func _process(delta: float) -> void:
+	mousePos = get_global_mouse_position();
+	if Input.is_action_just_pressed("placeObj"):
+		obj = basicExtractor.instantiate()
+		obj.global_position = mousePos
+		add_child(obj)
