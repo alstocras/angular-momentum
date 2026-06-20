12265dea254f6ca3730a3487e3d8a7b6f64bd6f2
alstocras <alstoperiment@gmail.com>
2026-06-20 05:59:22 +0000

    player cloning ig???

--- src/scripts/player.gd
+++ src/scripts/player.gd
@@ -1,13 +1,23 @@
 extends CharacterBody2D;
 @export var speed: float = 400;
 var direction: Vector2;
+var objPath = preload("res://src/scenes/player.tscn");
+var mainScene = preload("res://src/scenes/main.tscn")
+var mousePos;
+var obj;
 
 func _physics_process(delta: float) -> void:
+	mousePos = get_global_mouse_position();
 	direction = Input.get_vector("moveWest", "moveEast", "moveNorth", "moveSouth");
 	
 	if direction != Vector2.ZERO:
 		velocity = direction * speed;
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, speed);
+		
+	if Input.is_action_just_pressed("placeObj"):
+		obj = objPath.instantiate();
+		obj.global_position = mousePos;
+		add_child(obj)
 	
 	move_and_slide();
