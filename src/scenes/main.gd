8352a0c9d49ad8f3bd11e7ada166aa0f70555f77
alstocras <alstoperiment@gmail.com>
2026-06-20 13:06:43 +0000

    broken placement script

--- src/scenes/main.gd
+++ src/scenes/main.gd
@@ -3,11 +3,8 @@ extends Node2D;
 var basicExtractor = preload("res://src/scenes/extractor.tscn");
 var mousePos;
 var obj;
-@onready var playerScene = $Player
 
 func _process(delta: float) -> void:
-	mousePos = get_global_mouse_position();
+	mousePos = $TileMapLayer.get_local_mouse_position();
 	if Input.is_action_just_pressed("placeObj"):
-		obj = basicExtractor.instantiate()
-		obj.global_position = mousePos
-		add_child(obj)
+		$TileMapLayer.set_cell($TileMapLayer.local_to_map(mousePos), 1, Vector2i(-1, -1), 0)
