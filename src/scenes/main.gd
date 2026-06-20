bcef7cf6d0a8593df3a37daed1d8ce4aecb3b1e7
alstocras <alstoperiment@gmail.com>
2026-06-20 14:45:17 +0000

    remove buildings possible

--- src/scenes/main.gd
+++ src/scenes/main.gd
@@ -1,10 +1,14 @@
 extends Node2D;
 
 var mousePos;
+var activeCell;
 @onready var tMap = $Buildings
 
 func _process(delta: float) -> void:
 	mousePos = tMap.get_local_mouse_position();
+	activeCell = tMap.local_to_map(mousePos);
 	if Input.is_action_just_pressed("placeObj"):
 		if not tMap.get_cell_source_id(tMap.local_to_map(mousePos)) == 1:
-			tMap.set_cell(tMap.local_to_map(mousePos), 0, Vector2i(0, 0), 1)
+			tMap.set_cell(activeCell, 0, Vector2i(0, 0), 1)
+	if Input.is_action_just_pressed("removeObj"):
+		tMap.erase_cell(activeCell)
