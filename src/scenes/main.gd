e21d4057c98f2326facbc75aa3256d1ba68507fa
alstocras <alstoperiment@gmail.com>
2026-06-20 14:03:41 +0000

    well atleast its not colliding and flying

--- src/scenes/main.gd
+++ src/scenes/main.gd
@@ -5,4 +5,5 @@ var mousePos;
 func _process(delta: float) -> void:
 	mousePos = $TileMapLayer.get_local_mouse_position();
 	if Input.is_action_just_pressed("placeObj"):
-		$TileMapLayer.set_cell($TileMapLayer.local_to_map(mousePos), 1, Vector2i(0, 0), 1)
+		if not $TileMapLayer.get_cell_source_id($TileMapLayer.local_to_map(mousePos)) == 1:
+			$TileMapLayer.set_cell($TileMapLayer.local_to_map(mousePos), 1, Vector2i(0, 0), 1)
