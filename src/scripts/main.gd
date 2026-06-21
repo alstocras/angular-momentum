e494fc63b16620c2630424d5c0315f53bf092127
alstocras <alstoperiment@gmail.com>
2026-06-21 05:50:26 +0000

    broken ahh zoom mechanic

--- src/scripts/main.gd
+++ src/scripts/main.gd
@@ -2,8 +2,10 @@ extends Node2D;
 
 var mousePos;
 var activeCell;
-@onready var tMap = $Buildings
-@onready var terrain = $Terrain
+@onready var tMap = $Buildings;
+@onready var terrain = $Terrain;
+@onready var bosonLabel = $CanvasLayer/Bosons;
+var bosonCount: int;
 
 func _process(delta: float) -> void:
 	mousePos = tMap.get_local_mouse_position();
@@ -12,6 +14,8 @@ func _process(delta: float) -> void:
 		if not tMap.get_cell_source_id(activeCell) == 1:
 			tMap.set_cell(activeCell, 0, Vector2i(0, 0), 1)
 	if terrain.get_cell_source_id(activeCell) == 2 and tMap.get_cell_source_id(activeCell) == 0:
-			print("extracting...")
+			print("extracting...");
+			bosonCount += 1
 	if Input.is_action_just_pressed("removeObj"):
 		tMap.erase_cell(activeCell)
+	bosonLabel.text = "bosons: " + str(bosonCount)
