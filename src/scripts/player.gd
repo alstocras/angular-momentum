f6034ba04d374edfbc2d2f004a9715db5e2e9c7c
alstocras <alstoperiment@gmail.com>
2026-06-21 08:17:08 +0000

    attempt to revamp movement mechanics

--- src/scripts/player.gd
+++ src/scripts/player.gd
@@ -1,18 +1,13 @@
 extends CharacterBody2D;
 @export var speed: float = 400;
-var direction: Vector2;
-
-signal placeExtractor()
+@export var rotationSpeed = 3;
+var rotationDirection: float;
 
 func _physics_process(delta: float) -> void:
-	direction = Input.get_vector("moveWest", "moveEast", "moveNorth", "moveSouth");
+	var targetDirection = get_global_mouse_position() - global_position
+	var targetAngle = targetDirection.angle()
+	rotationDirection = Input.get_axis("moveWest", "moveEast");
 	
-	if direction != Vector2.ZERO:
-		velocity = direction * speed;
-	else:
-		velocity = velocity.move_toward(Vector2.ZERO, speed);
-		if Input.is_action_just_pressed("placeObj"):
-			placeExtractor.emit()
-			
+	rotation += rotationDirection * rotationSpeed * delta
 		
 	move_and_slide();
