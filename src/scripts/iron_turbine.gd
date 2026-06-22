3ba1fd8ba7781848fcdf1156054281a5c295fe05
alstocras <alstoperiment@gmail.com>
2026-06-22 15:55:24 +0000

    new sprite

--- src/scripts/iron_turbine.gd
+++ src/scripts/iron_turbine.gd
@@ -6,9 +6,9 @@ func _physics_process(delta: float) -> void:
 	var buildings = Global.buildingMap;
 	if Global.totalBosons >= 10:
 		for x in range(-lookRange, lookRange):
-			for y in range(-lookRange, lookRange):
-				var activeCellId := buildings.get_cell_source_id(Vector2i(x, y))
-				if activeCellId == 2:
+			for y in range(-lookRange, lookRange): 
+				if buildings.get_cell_source_id(Vector2i(x, y)) == 2:
 					print("making electricity");
-					$AnimatedSprite2D.play("generate");
+					if not $AnimatedSprite2D.is_playing():
+						$AnimatedSprite2D.play("generate");
 					
