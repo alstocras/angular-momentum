a27e69d34f8272953d54da44203e878efe53bf3a
alstocras <alstoperiment@gmail.com>
2026-06-26 06:45:39 +0000

    oooooooo cinema

--- src/scripts/credits.gd
+++ src/scripts/credits.gd
@@ -4,3 +4,6 @@ func _process(delta: float) -> void:
 	$VBoxContainer.position.y -= 25 * delta;
 	
 	$VBoxContainer/Stats.text = str(roundi(Global.totalIron)) + " iron, " + str(roundi(Global.totalBosons)) + " bosons, " + str(roundi(Global.totalFermions)) + " fermions"
+	
+	if Input.is_action_just_pressed("menu"):
+		get_tree().change_scene_to_file("res://src/scenes/main-menu.tscn");
