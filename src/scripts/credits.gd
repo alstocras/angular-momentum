8e80fdbbf80c4647f9e97baa4dd7a96aa2ae9b16
alstocras <alstoperiment@gmail.com>
2026-06-26 06:02:12 +0000

    music and godot

--- src/scripts/credits.gd
+++ src/scripts/credits.gd
@@ -3,4 +3,4 @@ extends Node2D
 func _process(delta: float) -> void:
 	$VBoxContainer.position.y -= 25 * delta;
 	
-	$VBoxContainer/Stats.text = str(roundi(Global.totalIron)) + " Fe, " + str(roundi(Global.totalBosons)) + " bosons, " + str(roundi(Global.totalFermions)) + " fermions"
+	$VBoxContainer/Stats.text = str(roundi(Global.totalIron)) + " iron, " + str(roundi(Global.totalBosons)) + " bosons, " + str(roundi(Global.totalFermions)) + " fermions"
